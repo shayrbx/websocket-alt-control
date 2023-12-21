308d6e085ea9c809e8c009b3a55435b47db4e3d8
Shay <111154756+shayrbx@users.noreply.github.com>
2023-12-20 23:55:08 -0600

    Fix arceus x execution

--- autoexec.lua
+++ autoexec.lua
@@ -1,15 +1,22 @@
 -- Replace 192.168.116.133 with the local IP to the device running the server as long as it's on the same network
-local Socket = WebSocket.connect("ws://192.168.116.133:3621")
+local Socket = WebSocket.connect("ws://192.168.4.20:3621")
 local Thread = coroutine.running()
 
--- runcode is basically just task.spawn(loadstring, param1)
 -- When the ws server sends a message to us, it means we need to execute the code. There is no message sent to us from the server unless "executecode" was sent to the server
-Socket.OnMessage:Connect(runcode)
+Socket.OnMessage:Connect(function(Code)
+	local Func, Error = loadstring(Code)
+
+	if not Func then 
+		warn(Error)
+	else
+		task.spawn(Func)
+	end
+end)
 Socket.OnClose:Connect(function() -- If the server crashes, we want to be able to alert the user. This also keeps the script from infinitely waiting lol
 	coroutine.resume(Thread)
 end)
 
--- The socket can get GARBAGE COLLECTED, so this only lets the end of the script execute if the server dies. It's like edging, but better!
+-- The socket can get GARBAGE COLLECTED, so this only lets the end of the script execute if the server dies.
 coroutine.yield()
 
 print("Server died!")
